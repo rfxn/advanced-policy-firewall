ffe790c9464698d637849ddeec232d8c73cd3076
tomdoyle87 <36793507+tomdoyle87@users.noreply.github.com>
2019-07-14 09:51:21 +0100

    wget and ifconfig dependency
    Systemctl will now fail if wget & ifconfig are not installed. As both are not installed by default in centos.

--- files/apf-start.sh
+++ files/apf-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /usr/local/sbin/apf --start &> /tmp/check-apf
-if egrep 'unable to load iptables module|timed out while attempting to gain lock|could not process allow_hosts|could not process deny_hosts|apf does not appear to have rules loaded|could not verify that interface|trust rules unchanged since last refresh' /tmp/check-apf; then
+if egrep 'unable to load iptables module|timed out while attempting to gain lock|could not process allow_hosts|could not process deny_hosts|apf does not appear to have rules loaded|could not verify that interface|trust rules unchanged since last refresh|wget binary not found|route: No such file or directory' /tmp/check-apf; then
 	/usr/local/sbin/apf --stop
 	echo "APF Aborted"
 	exit 1
