2f87e829ca014e4ea727836a70d05f802324c55a
tomdoyle87 <36793507+tomdoyle87@users.noreply.github.com>
2019-07-14 21:15:48 +0100

    removed redundancy from grep

--- files/apf-start.sh
+++ files/apf-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /usr/local/sbin/apf --start
 sleep 1
-if tail -n 4 /var/log/messages | egrep "^($(date -d '+1 hour' '+%H')|$(date '+%H')):[0-9]{2}:[0-9]{2}' 'unable to load iptables module|timed out while attempting to gain lock|could not process allow_hosts|could not process deny_hosts|apf does not appear to have rules loaded|could not verify that interface|trust rules unchanged since last refresh|wget binary not found|route: No such file or directory'"; then
+if tail -n 4 /var/log/messages | egrep 'unable to load iptables module|timed out while attempting to gain lock|could not process allow_hosts|could not process deny_hosts|apf does not appear to have rules loaded|could not verify that interface|trust rules unchanged since last refresh|wget binary not found|route: No such file or directory'; then
         /usr/local/sbin/apf --stop
         echo "APF Aborted"
         exit 1
